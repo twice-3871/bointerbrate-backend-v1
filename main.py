fae44b06a3b295da43d0f68795bff4f7849e1a11
badlandschugs <robloxfan2017@icloud.com>
2026-06-17 20:59:59 -0400

    debugging localhost, fixing ux on the frontend

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://test.bointerbrate.meme"],
+    allow_origins=["https://test.bointerbrate.meme", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
